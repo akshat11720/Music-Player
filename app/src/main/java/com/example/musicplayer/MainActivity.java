7516b7f64c0ff86cd7dd914dfb02677cce87f338
Akshat <akshatkinger17@gmail.com>
2021-07-14 13:20:14 +0530

    Music Player App

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listView);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                        //Toast.makeText(MainActivity.this, "Runtime permission given", Toast.LENGTH_SHORT).show();
+         ArrayList<File> mySongs=fetchSongs(Environment.getExternalStorageDirectory());//this will give ous
+         //external storage directory saare ke saare songs arraylist mySongs mein aajayenge
+          String []items=new String[mySongs.size()];
+          for(int i=0;i<mySongs.size();i++){
+              items[i]=mySongs.get(i).getName().replace(".mp3","");
+
+          }
+                        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,items);
+          listView.setAdapter(adapter);
+          listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                  Intent intent=new Intent(MainActivity.this,PlaySong.class);
+                  String currentSong=listView.getItemAtPosition(position).toString();
+                  intent.putExtra("songList",mySongs);
+                  intent.putExtra("currentSong",currentSong);
+                  intent.putExtra("position",position);
+                  startActivity(intent);
+
+              }
+          });
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                     permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+    // ye method humein ek folder mein se humko saari mp3 files return karke dega
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList=new ArrayList();
+        File []songs =file.listFiles();
+        //ye hamari saaari files ko list kar dega iss directory (file) ke andar jitni
+        //bhi files h unko list kar dega ab hum isme recursively jayenge aur iske andar ki saari ki saari files ko lekar
+        // aajyenge aur iss arrayList mein add kar denge
+        if(songs !=null){
+            for( File myFile: songs) {
+                if (!myFile.isHidden() && myFile.isDirectory()) {
+                    arrayList.addAll(fetchSongs(myFile));//ye jo arraylist return karega uske andar jitne bhi
+                    //items h unn sabko iss arraylist mein add kar dega
+                } else {
+                    if (myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(("."))) {
+                        arrayList.add(myFile);
+                    }
+                }
+               }
+            }
+            return arrayList;
+            // saaari ki saari jo humne songs fetch kiye h yaanki ki saari ki saari files ya directory jo humne fetch
+            //kari h unko iterate karo
+        }
+    }
+
